c25207e727afef604f05cde1d7a5f4c159cf8fad
Max Yermakhanov <max@automagically.io>
2023-12-05 11:33:48 -0500

    fix bug

--- parse_expenses.py
+++ parse_expenses.py
@@ -1,6 +1,6 @@
 import datetime
 
-def parse_expenses (expenses_string) :
+def parse_expenses(expenses_string):
     """Parse the list of expenses and return the
     list of triples (date, value, currency).
     Ignore lines starting with #.
@@ -14,18 +14,18 @@ def parse_expenses (expenses_string) :
     for line in expenses_string.splitlines():
         if line.startswith("#"):
             continue
-        date, value, currency = line.split(",")
-        expenses.append((float (value),
-                        currency,
-                        datetime.datetime.strptime(date, "%Y-%m-%d")))
+        date, value, currency = line.split(" ")
+        expenses.append((float(value),
+                         currency,
+                         datetime.datetime.strptime(date, "%Y-%m-%d")))
     return expenses
 
 expenses_data = '''2023-01-02 -34.01 USD
 2023-01-03 2.59 DKK
 2023-01-03 -2.72 EUR'''
 
-expenses = parse_expenses (expenses_data)
+expenses = parse_expenses(expenses_data)
 for expense in expenses:
-    print (f'{expense[0]} {expense[1]} {expense[2]}')
+    print(f'{expense[0]} {expense[1]} {expense[2]}')
 
 
--- utils.py
+++ utils.py
@@ -1,18 +1,28 @@
 import re
 
-e_regex = r'^[\w\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$'
-p_regex = r'^(\+)?1?\d{9,15}$'
-s_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+# Regular expression for email validation
+email_regex = r'^[\w\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$'
 
-def checkvalid(text, regex):
-    if re.search(regex, text):
-        return True
-    else:
-        return False
+# Regular expression for phone number validation
+phone_regex = r'^(\+)?1?\d{9,15}$'
 
-if __name__	== '__main__':
-    print(("valid", "invalid")[checkvalid('',e_regex)])
-    print(("valid", "invalid")[checkvalid('',p_regex)])
-    print(("valid", "invalid")[checkvalid('',s_regex)])
+# Regular expression for strong password validation
+password_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
 
+def is_valid_email(email):
+    """Check if the provided email is valid."""
+    return bool(re.search(email_regex, email))
 
+def is_valid_phone(phone):
+    """Check if the provided phone number is valid."""
+    return bool(re.search(phone_regex, phone))
+
+def is_valid_password(password):
+    """Check if the provided password is valid."""
+    return bool(re.search(password_regex, password))
+
+if __name__ == '__main__':
+    # Test the validation functions
+    print('Email:', 'valid' if is_valid_email('') else 'invalid')
+    print('Phone:', 'valid' if is_valid_phone('') else 'invalid')
+    print('Password:', 'valid' if is_valid_password('') else 'invalid')
