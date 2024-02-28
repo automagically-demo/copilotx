8cf7bf537aca3da7ae7f297317e62a8edee93d31
Max Yermakhanov <max@automagically.io>
2024-02-27 20:34:17 -0500

    clean

--- utils.py
+++ utils.py
@@ -14,3 +14,5 @@ if __name__	== '__main__':
     print(("valid", "invalid")[checkvalid('',e_regex)])
     print(("valid", "invalid")[checkvalid('',p_regex)])
     print(("valid", "invalid")[checkvalid('',s_regex)])
+
+
