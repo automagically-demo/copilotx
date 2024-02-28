871e4f3d07bbfecfdecd1335d1b6983f1cf24c09
Max Yermakhanov <max@automagically.io>
2024-02-27 20:33:20 -0500

    undo utils.py changes

--- utils.py
+++ utils.py
@@ -1,43 +1,17 @@
 import re
 
-def is_valid_email(email):
-    """
-    Validate an email address.
+e_regex = r'^[\w\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$'
+p_regex = r'^(\+)?1?\d{9,15}$'
+s_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
 
-    Parameters:
-    email (str): The email address to validate.
+def checkvalid(text, regex):
+    if re.search(regex, text):
+        return True
+    else:
+        return False
 
-    Returns:
-    bool: True if the email address is valid, False otherwise.
-    """
-    e_regex = r'^[\w\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$'
-    return bool(re.search(e_regex, email))
+if __name__	== '__main__':
+    print(("valid", "invalid")[checkvalid('',e_regex)])
+    print(("valid", "invalid")[checkvalid('',p_regex)])
+    print(("valid", "invalid")[checkvalid('',s_regex)])
 
-
-def is_valid_phone(phone):
-    """
-    Validate a phone number.
-
-    Parameters:
-    phone (str): The phone number to validate.
-
-    Returns:
-    bool: True if the phone number is valid, False otherwise.
-    """
-    p_regex = r'^(\+)?1?\d{9,15}$'
-    return bool(re.search(p_regex, phone))
-
-
-def is_valid_password(password):
-    """
-    Validate a password. The password must contain at least one lowercase letter,
-    one uppercase letter, one digit, and one special character. It must be at least 8 characters long.
-
-    Parameters:
-    password (str): The password to validate.
-
-    Returns:
-    bool: True if the password is valid, False otherwise.
-    """
-    s_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
-    return bool(re.search(s_regex, password))
