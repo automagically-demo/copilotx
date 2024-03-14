cd71addaed895b61c1e02741e3e7af2c70b2703b
Max Yermakhanov <max@automagically.io>
2024-03-14 18:23:27 -0400

    Refactor validation functions and add print statements

--- utils.py
+++ utils.py
@@ -1,18 +1,19 @@
 import re
 
-e_regex = r'^[\w\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$'
-p_regex = r'^(\+)?1?\d{9,15}$'
-s_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
+email_regex = r'^[\w\.\+\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$'
+phone_regex = r'^(\+)?1?\d{9,15}$'
+password_regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
 
-def checkvalid(text, regex):
-    if re.search(regex, text):
-        return True
-    else:
-        return False
+def is_valid(text, regex):
+    """Check if the text matches the provided regex."""
+    return bool(re.search(regex, text))
 
-if __name__	== '__main__':
-    print(("valid", "invalid")[checkvalid('',e_regex)])
-    print(("valid", "invalid")[checkvalid('',p_regex)])
-    print(("valid", "invalid")[checkvalid('',s_regex)])
+def print_validation_result(is_valid, input_type):
+    """Print whether the input is valid or invalid."""
+    result = "valid" if is_valid else "invalid"
+    print(f"The {input_type} is {result}.")
 
-
+if __name__ == '__main__':
+    print_validation_result(is_valid('', email_regex), "email")
+    print_validation_result(is_valid('', phone_regex), "phone number")
+    print_validation_result(is_valid('', password_regex), "password")
